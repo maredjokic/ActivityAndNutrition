e793873622aa18b36f7ca16cca122bfc45963b8c
Mare <mare160696@gmail.com>
2020-06-27 00:04:14 +0200

    navbar on left side, table on ishrana

--- frontend/src/Layout.vue
+++ frontend/src/Layout.vue
@@ -1,45 +1,70 @@
 <template>
 <div id="app">
 <v-app>
-  <v-app-bar short app color="primary">
-    <a><router-link v-bind:to="'/'" exact>
-      <v-btn large text class="ma-2" color="white">
-        Pocetna
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
-    </router-link></a>
-    <a><router-link v-bind:to="'/Pregled'" exact>
-      <v-btn large text class="ma-2" color="white">
-        Pregled
-        <v-icon>mdi-ballot-outline</v-icon>
-      </v-btn>
-    </router-link></a>
-    <a><router-link v-bind:to="'/Trcanje'" exact>
-      <v-btn large text class="ma-2" color="white">
-        Trcanje
-        <v-icon>mdi-run</v-icon>
-      </v-btn>
-    </router-link></a>
-    <a><router-link v-bind:to="'/PlanTrcanja'" exact>
-      <v-btn large text class="ma-2" color="white">
-        Plan Trcanja
-        <v-icon>mdi-notebook</v-icon>
-      </v-btn>
-    </router-link></a>
+  <v-app-bar absolute short app color="primary">
+    <v-app-bar-nav-icon @click="drawer = !drawer" ></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-icon class="mb-1 mr-1">mdi-run</v-icon>
+    <b>ACTIVITY AND NUTRITION</b>
+    <v-icon class="mb-1 ml-1">mdi-food-apple-outline</v-icon>
     <v-spacer></v-spacer>
     <a><router-link v-bind:to="'/Prijava'" exact>
-      <v-btn large text class="ma-2" color="white">
-        Prijava
+      <v-btn medium text class="ma-2" color="white">
         <v-icon>mdi-login</v-icon>
       </v-btn>
     </router-link></a>
     <a><router-link v-bind:to="'/Registracija'" exact>
-      <v-btn large text class="ma-2" color="white">
-        Registracija
+      <v-btn medium text class="ma-2" color="white">
         <v-icon>mdi-account-plus</v-icon>
       </v-btn>
     </router-link></a>
   </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+
+    <router-link class="routerLink" v-bind:to="'/'" exact>
+      <v-btn medium text class="ma-2" color="white">
+        Pocetna
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </router-link>
+   <router-link class="routerLink" v-bind:to="'/Pregled'" exact>
+      <v-btn medium text class="ma-2" color="white">
+        Pregled
+        <v-icon>mdi-ballot-outline</v-icon>
+      </v-btn>
+    </router-link>
+   <router-link class="routerLink" v-bind:to="'/Trcanje'" exact>
+      <v-btn medium text class="ma-2" color="white">
+        Trcanje
+        <v-icon>mdi-run</v-icon>
+      </v-btn>
+    </router-link>
+   <router-link class="routerLink" v-bind:to="'/PlanTrcanja'" exact>
+      <v-btn medium text class="ma-2" color="white">
+        Plan Trcanja
+        <v-icon>mdi-notebook</v-icon>
+      </v-btn>
+    </router-link>
+    <v-spacer></v-spacer>
+   <router-link class="routerLink" v-bind:to="'/Prijava'" exact>
+      <v-btn medium text class="ma-2" color="white">
+        Prijava
+        <v-icon>mdi-login</v-icon>
+      </v-btn>
+    </router-link>
+    <router-link class="routerLink" v-bind:to="'/Registracija'" exact>
+      <v-btn medium text class="ma-2" color="white">
+        Registracija
+        <v-icon>mdi-account-plus</v-icon>
+      </v-btn>
+    </router-link>
+
+    </v-navigation-drawer>
 
   <!-- Sizes your content based upon application components -->
   <v-content>
@@ -65,7 +90,8 @@ export default {
     name: 'Layot',
     data () {
         return {
-            activeBtn: [0, 1, 2]
+            activeBtn: [0, 1, 2],
+            drawer: false
         };
     },
     components: {
@@ -74,4 +100,7 @@ export default {
 </script>
 
 <style>
+.routerLink{
+    text-decoration: none;
+}
 </style>
--- frontend/src/views/Pocetna.vue
+++ frontend/src/views/Pocetna.vue
@@ -1,8 +1,39 @@
 <template>
   <div>
-      <div id="chart">
-        <apexchart ref="pie" type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
-      </div>
+      <v-tabs
+        v-model="tab"
+        background-color="primary"
+        dark
+        flat
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item.tab"
+        >
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item.tab"
+        >
+          <v-card flat>
+            <div v-show="item.tab === 'Pregled'" id="chart">
+                <apexchart ref="pie" type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
+            </div>
+            <div v-show="item.tab === 'Ishrana'">
+                <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :items-per-page="5"
+                class="elevation-1"
+              ></v-data-table>
+            </div>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
   </div>
 </template>
 
@@ -14,6 +45,12 @@ export default {
     },
     data () {
         return {
+            tab: null,
+            items: [
+                { tab: 'Ishrana', content: 'Tab 1 Content' },
+                { tab: 'Aktivnost', content: 'Tab 2 Content' },
+                { tab: 'Pregled', content: 'Tab 3 Content' }
+            ],
             series: [44, 55, 13],
             chartOptions: {
                 theme: {
@@ -38,7 +75,95 @@ export default {
                         }
                     }
                 }]
-            }
+            },
+            headers: [
+                {
+                    text: 'Dessert (100g serving)',
+                    align: 'start',
+                    sortable: false,
+                    value: 'name'
+                },
+                { text: 'Calories', value: 'calories' },
+                { text: 'Fat (g)', value: 'fat' },
+                { text: 'Carbs (g)', value: 'carbs' },
+                { text: 'Protein (g)', value: 'protein' },
+                { text: 'Iron (%)', value: 'iron' }
+            ],
+            desserts:
+            [
+                {
+                    name: 'Frozen Yogurt',
+                    calories: 159,
+                    fat: 6.0,
+                    carbs: 24,
+                    protein: 4.0,
+                    iron: '1%'
+                },
+                {
+                    name: 'Ice cream sandwich',
+                    calories: 237,
+                    fat: 9.0,
+                    carbs: 37,
+                    protein: 4.3,
+                    iron: '1%'
+                },
+                {
+                    name: 'Cupcake',
+                    calories: 305,
+                    fat: 3.7,
+                    carbs: 67,
+                    protein: 4.3,
+                    iron: '8%'
+                },
+                {
+                    name: 'Gingerbread',
+                    calories: 356,
+                    fat: 16.0,
+                    carbs: 49,
+                    protein: 3.9,
+                    iron: '16%'
+                },
+                {
+                    name: 'Jelly bean',
+                    calories: 375,
+                    fat: 0.0,
+                    carbs: 94,
+                    protein: 0.0,
+                    iron: '0%'
+                },
+                {
+                    name: 'Lollipop',
+                    calories: 392,
+                    fat: 0.2,
+                    carbs: 98,
+                    protein: 0,
+                    iron: '2%'
+                },
+                {
+                    name: 'Honeycomb',
+                    calories: 408,
+                    fat: 3.2,
+                    carbs: 87,
+                    protein: 6.5,
+                    iron: '45%'
+                },
+                {
+                    name: 'Donut',
+                    calories: 452,
+                    fat: 25.0,
+                    carbs: 51,
+                    protein: 4.9,
+                    iron: '22%'
+                },
+                {
+                    name: 'KitKat',
+                    calories: 518,
+                    fat: 26.0,
+                    carbs: 65,
+                    protein: 7,
+                    iron: '6%'
+                }
+            ]
         };
     },
     mounted: function () {

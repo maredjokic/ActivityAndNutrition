7af8ac62e6f40caa4f3abb0147244127c36c2fe9
maredjokic <mare160696@gmail.com>
2022-12-24 19:54:52 +0100

    material ui and app bar

--- running-plan-react/src/App.js
+++ running-plan-react/src/App.js
@@ -1,18 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from "./routes/Home"
 import About from "./routes/About"
 import Contact from "./routes/Contact"
+import AppBar from "@mui/material/AppBar";
+import Toolbar from '@mui/material/Toolbar';
+import Button from "@mui/material/Button";
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <Route path="/" element={ <Home/> } />
-      <Route path="about" element={ <About/> } />
-      <Route path="contact" element={ <Contact/> } />
-    </Routes>
+      <AppBar display="flex" position="static">
+        <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 0 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Training plan app
+            </Typography>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Register</Button>
+          </Toolbar>
+      </AppBar>
+      <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="about" element={ <About/> } />
+          <Route path="contact" element={ <Contact/> } />
+        </Routes>
   </div>
   );
 }
